b6f37c85e710a57f3a3581bb3f4d975e0933f3c0
Milan Ricoul <mricoul@beapi.fr>
2026-06-23 13:41:38 +0000

    Release version 1.2.0

---
+++ build/blockparty-iframe/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '093d55f2a1fecf2a3223');
---
+++ build/blocks-manifest.php
@@ -0,0 +1,63 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'blockparty-iframe' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'blockparty/iframe',
+		'version' => '1.2.0',
+		'title' => 'Iframe',
+		'category' => 'widgets',
+		'description' => 'Display an embedded frame.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'align' => array(
+				'wide',
+				'full'
+			),
+			'dimensions' => array(
+				'aspectRatio' => true,
+				'height' => true,
+				'width' => true
+			),
+			'html' => false
+		),
+		'attributes' => array(
+			'lazyload' => array(
+				'type' => 'boolean',
+				'default' => false
+			),
+			'title' => array(
+				'type' => 'string',
+				'default' => ''
+			),
+			'url' => array(
+				'type' => 'string',
+				'default' => ''
+			),
+			'iframeAttributes' => array(
+				'type' => 'array',
+				'default' => array(
+					
+				),
+				'items' => array(
+					'type' => 'object',
+					'properties' => array(
+						'key' => array(
+							'type' => 'string'
+						),
+						'value' => array(
+							'type' => 'string'
+						)
+					)
+				)
+			)
+		),
+		'textdomain' => 'blockparty-iframe',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css'
+	)
+);
